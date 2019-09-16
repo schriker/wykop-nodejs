a0b97244deb804c6e1751300bfd7ccc8cb66941b
schriker <janekmachine@gmail.com>
2019-09-16 10:03:59 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,96 @@
+const axios = require('axios')
+const qs = require('querystring')
+const md5 = require('md5')
+
+type apiConfig = {
+  appSecret: string,
+  appKey: string
+}
+
+type requestMethod = 'GET' | 'POST'
+
+type paramsObject = {
+  [key: string]: string | number
+}
+
+type requestObject = {
+  requestMethod: requestMethod,
+  url: string,
+  apiSign: string,
+  postParams?: object
+}
+
+type requestData = {
+  requestMethod: requestMethod, 
+  apiParams: string[], 
+  namedParams?: paramsObject | undefined, 
+  postParams?: {[key: string]: string | number}
+}
+
+class Wykop {
+  private appSecret: string
+  private appKey: string
+  public userKey: string = ''
+  
+  constructor ({ appSecret, appKey }: apiConfig) {
+    this.appSecret = appSecret
+    this.appKey = appKey
+  }
+
+  private createUrl (apiParams: string[], namedParams: paramsObject | undefined): string {
+    let joinedNamedParams: string = ''
+    const baseUrl = `https://a2.wykop.pl/${apiParams.join('/')}/appkey/${this.appKey}/userkey/${this.userKey}`
+    if (namedParams) {
+      joinedNamedParams = Object.entries(namedParams).map(([ key, value ]) => `${key}/${value}`).join('/')
+    }
+    return `${baseUrl}/${joinedNamedParams}`
+  }
+
+  private createApiSign (requestUrl: string, postParams?: {[key: string]: string | number}): string {
+    let postParamsString: string = ''
+    if (postParams) {
+      postParamsString = Object.keys(postParams).map((key: string) => postParams[key]).join(',')
+    }
+    return md5(`${this.appSecret}${requestUrl}${postParamsString}`)
+  }
+
+  private  makeRequest ({ requestMethod, url, apiSign, postParams }: requestObject): Promise<any> {
+    return axios({
+      method: requestMethod,
+      url: url,
+      headers: {
+        'apisign': apiSign,
+        'User-Agent': 'wykop-nodejs',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: qs.stringify(postParams)
+      })
+  }
+
+  public request({ requestMethod, apiParams, namedParams, postParams }: requestData): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const url = this.createUrl(apiParams, namedParams)
+      const apiSign = this.createApiSign(url, postParams)
+      const request: requestObject = {
+        requestMethod: requestMethod,
+        url: url,
+        apiSign: apiSign,
+        postParams: postParams
+      }
+      this.makeRequest(request)
+        .then((res) => {
+          if (res.data.data === null) {
+            reject(res)
+          } else if (apiParams[0].toLowerCase() === 'login') {
+            this.userKey = res.data.data.userkey
+            resolve(res.data)
+          } else {
+            resolve(res.data)
+          }
+        })
+        .catch((err) => reject(err))
+    })
+  }
+}
+
+module.exports = Wykop
